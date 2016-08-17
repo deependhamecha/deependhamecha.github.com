6ffd7f275c91c2720ee8a1b406a96c2a0496daa8
Deepen Dhamecha <deependhamecha@gmail.com>
2016-08-17 18:24:29 +0530

    Added Header and Menu Background

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.menu-background').on ('mousewheel', function (e) {
+	    var delta = e.originalEvent.wheelDelta;
+	    
+	    if (delta < 0) {
+	        
+	    } else if (delta > 0) {
+	        
+	    }
+	});
+});
